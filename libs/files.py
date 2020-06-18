a596f657f1315d0037ec82ef191680c0ff29ff50
Joao Koritar <jakoritarleite@gmail.com>
2020-06-18 19:21:14 +0000

    add: function annotation to static functions

--- libs/files.py
+++ libs/files.py
@@ -5,7 +5,7 @@ class Manager(object):
         self.rename = lambda _file, tp: rename(_file, _file + '.pcrp') if not tp else rename(_file, _file.replace('.pcrp', ''))
 
     @staticmethod
-    def __read__(FILE):
+    def __read__(FILE: 'path/filename') -> 'raw data in file bytes':
         with open(FILE, 'rb') as _file:
             inFile = _file.read()
             _file.close()
@@ -13,7 +13,7 @@ class Manager(object):
         return inFile
 
     @staticmethod
-    def __write__(FILE, CONTENT):
+    def __write__(FILE: 'path/filename', CONTENT: 'raw data file bytes'):
         with open(FILE, 'wb') as _file:
             _file.write(CONTENT)
             _file.close()
