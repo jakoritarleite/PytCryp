448ac1acb6e2a9eca8f1e88ee160fca59b383661
jakoritarleite <jakoritarleite@gmail.com>
2020-06-18 03:22:59 +0000

    add: configs file

---
+++ libs/confs.py
@@ -0,0 +1,7 @@
+class Configurations(object):
+    def __init__(self):
+        self.Path = {
+                'win32': 'C:\\Users\\',
+                'linux': '/home/',
+                'darwin': '/Users/'
+        }
